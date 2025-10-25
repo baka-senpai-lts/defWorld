225592af248f9d6c890d9cc52b2c400e40790307
baka <baka.senpai.lts@gmail.com>
2025-10-26 00:58:06 +0300

    add: MouseMoveEvent

---
+++ src/events/mouse_move.hpp
@@ -0,0 +1,34 @@
+#ifndef MOUSE_MOVE_H_
+#define MOUSE_MOVE_H_
+
+#include "event.hpp"
+#include <raylib.h>
+
+namespace game::event {
+
+class MouseMoveEvent : public Event {
+private:
+  Vector2 pos;
+
+public:
+  MouseMoveEvent() {
+    type = EventType::MOUSE_MOVE;
+    pos.x = 0;
+    pos.y = 0;
+  }
+
+  MouseMoveEvent(Vector2 pos) : game::event::MouseMoveEvent() {
+    this->pos.x = pos.x;
+    this->pos.y = pos.y;
+  }
+
+  ~MouseMoveEvent() {}
+
+  Vector2 getPos() const { return this->pos; }
+  int getX() const { return pos.x; }
+  int getY() const { return pos.y; }
+};
+
+} // namespace game::event
+
+#endif // MOUSE_MOVE_H_
