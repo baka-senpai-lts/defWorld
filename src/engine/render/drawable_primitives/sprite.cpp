141c821f36a3d3abcb58d82a479c6709ee9c741d
baka <baka.senpai.lts@gmail.com>
2025-10-28 18:33:02 +0200

    add: Sprite drawable primitive

---
+++ src/engine/render/drawable_primitives/sprite.cpp
@@ -0,0 +1,15 @@
+#include "sprite.hpp"
+#include <cmath>
+#include <raylib.h>
+
+namespace engine::render {
+
+void Sprite::draw() const {
+  Rectangle sourceRec = {0, 0, (float)texture.width, (float)texture.height};
+  Rectangle destRec = {pos.x, pos.y, texture.width * scale,
+                       texture.height * scale};
+
+  DrawTexturePro(texture, sourceRec, destRec, origin, rotation, tint);
+}
+
+} // namespace engine::render
---
+++ src/engine/render/drawable_primitives/sprite.hpp
@@ -0,0 +1,52 @@
+#ifndef SPRITE_H_
+#define SPRITE_H_
+
+#include "../../log/logger.hpp"
+#include "../drawable.hpp"
+
+#include <cmath>
+#include <raylib.h>
+
+namespace engine::render {
+
+class Sprite : public Drawable {
+private:
+  Texture texture;
+  float rotation;
+  float scale;
+  Color tint;
+  Vector2 origin;
+
+public:
+  Sprite(Texture texture, Vector2 pos, float rotation = 0, Color tint = WHITE,
+         float scale = 1, int z = 0)
+      : Drawable(pos, z), rotation(fmod(rotation, 360)), scale(scale),
+        texture(texture), tint(tint) {
+    MT_LOG_DEBUG("Initialized sprite with texture size {}x{}", texture.height,
+                 texture.width);
+
+    // Default to center
+    origin = {(float)texture.width * scale / 2,
+              (float)texture.height * scale / 2};
+  }
+
+  ~Sprite() override {};
+
+  void setTexture(Texture texture) { this->texture = texture; }
+  void setRotation(float rotation) { this->rotation = fmod(rotation, 360); }
+  void setScale(float scale) { this->scale = scale; }
+  void setTint(Color tint) { this->tint = tint; }
+  void setOrigin(Vector2 origin) { this->origin = origin; }
+
+  inline Texture getTexture() const { return texture; }
+  inline float getRotation() const { return rotation; }
+  inline float getScale() const { return scale; }
+  inline Color getTint() const { return tint; }
+  inline Vector2 getOrigin() const { return origin; }
+
+  void draw() const override;
+};
+
+} // namespace engine::render
+
+#endif // SPRITE_H_
