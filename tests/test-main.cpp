cf4dc22a9987dfbbcf3351b85659e4ec7b21bd61
baka <baka.senpai.lts@gmail.com>
2025-10-29 02:17:18 +0200

    add: joke

--- tests/test-main.cpp
+++ tests/test-main.cpp
@@ -1,1 +1,17 @@
 #include "sanity-test.hpp"
+
+// You might think that I forgot about unit testing. The truth is that suddenly
+// in rendering part of engine there is literally nothing to test. So here is a
+// joke for you while you are waiting for tests:
+//
+// Появился, значит, в Зоне Черный Сталкер. К лагерю ночью повадился приходить
+// и там сует руку в палатку и говорит: "Водииички попиииить", а если не дашь
+// хлебнуть или наружу полезешь, пришибет! А раз мужик один решил пошутить:
+// вылез тихо из палатки, надел кожаную перчатку, и полез к соседям в палатку.
+// Полез и попрошайничает жалостно:
+// — Водииички, водииички попить…
+// А тут из палатки навстречу высовывается рука и за горло его… Цап! И такой
+// сиплый голосок отзывается тихонько:
+// — А тебе моя водичка зачем нужна?!
+//
+// TODO: remove joke after I get some meaningful tests
